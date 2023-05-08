988b369598a71cb762e5daf829fb9f1f7884003d
mysoyul <vega2k_@naver.com>
2023-05-08 14:30:38 +0900

    TodoInput update

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="inputBox">
-        <input type="text" :value="newTodoItem" @input="handleInput">
-        <button @click="addTodo">추가</button>
+    <div class="inputBox shadow">
+        <input type="text" :value="newTodoItem" @input="handleInput" @keyup.enter="addTodo">
+        <span class="addContainer" @click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
     </div>
 </template>
 
 <script setup>
-import { ref, defineEmits } from 'vue';
+import { ref, defineEmits } from 'vue'
 
 const newTodoItem = ref("")
 const emit = defineEmits(["input:todo"])
@@ -20,6 +22,10 @@ const handleInput = (event) => {
 const addTodo = () => {
     const todoItem = newTodoItem.value
     localStorage.setItem(todoItem, todoItem)
+    clearInput()
+}
+
+const clearInput = () => {
     newTodoItem.value = ""
 }
 </script>
