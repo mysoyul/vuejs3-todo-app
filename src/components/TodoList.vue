8fba7578de7adf91b58bb86b9c1acda15027134a
mysoyul <vega2k_@naver.com>
2023-05-08 14:40:29 +0900

    TodoList update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,9 +5,19 @@
 </template>
 
 <script setup>
+import { ref, onBeforeMount } from 'vue'
+const todoItems = ref([])
+onBeforeMount(() => {
+    console.log('mounted in the composition api!')
+    if (localStorage.length > 0) {
+        for (var i = 0; i < localStorage.length; i++) {
+            const storageValue = localStorage.key(i)
+            todoItems.value.push(storageValue)
+        }
+    }
+    console.log(todoItems.value)
+})
 
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
