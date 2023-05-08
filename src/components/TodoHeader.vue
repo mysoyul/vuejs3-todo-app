f452d0c5c7921c734a05fec0bbc26ca0f28705a7
mysoyul <vega2k_@naver.com>
2023-05-08 13:36:15 +0900

    TodoHeader update

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-
+        <header>
+            <h1>TODO it!</h1>
+        </header>
     </div>
 </template>
 
 <script>
 export default {
-    setup () {
-        
+    setup() {
+
 
         return {}
     }
@@ -15,5 +17,9 @@ export default {
 </script>
 
 <style scoped>
-
+h1 {
+    color: #2F3852;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
 </style>
