12d0d5fa15ea4cda12f5170396fd4a3b75cc7959
mysoyul <vega2k_@naver.com>
2023-05-08 14:47:45 +0900

    TodoList update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <ul>
-            <li v-for="(item,idx) in todoItems" :key="idx" >
+            <li v-for="(item, idx) in todoItems" :key="idx">
                 {{ item }}
             </li>
         </ul>
@@ -24,39 +24,48 @@ onBeforeMount(() => {
     console.log(todoItems.value)
 })
 
+const removeTodo = (todoItem, index) => {
+    localStorage.removeItem(todoItem)
+    todoItems.value.splice(index, 1)
+}
+
 </script>
 
 <style scoped>
 ul {
-  list-style-type: none;
-  padding-left: 0px;
-  margin-top: 0;
-  text-align: left;
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0;
+    text-align: left;
 }
+
 li {
-  display: flex;
-  min-height: 50px;
-  height: 50px;
-  line-height: 50px;
-  margin: 0.5rem 0;
-  padding: 0 0.9rem;
-  background: white;
-  border-radius: 5px;
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
 }
+
 .removeBtn {
-  margin-left: auto;
-  color: #de4343;
+    margin-left: auto;
+    color: #de4343;
 }
+
 .checkBtn {
-  line-height: 45px;
-  color: #62acde;
-  margin-right: 5px;
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
 }
+
 .checkBtnCompleted {
-  color: #b3adad;
+    color: #b3adad;
 }
+
 .textCompleted {
-  text-decoration: line-through;
-  color: #b3adad;
-}
-</style>
+    text-decoration: line-through;
+    color: #b3adad;
+}</style>
