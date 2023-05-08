1eae139a5c5cb0575a5ebe4b9c1a8aa4106e2f13
mysoyul <vega2k_@naver.com>
2023-05-08 14:43:50 +0900

    TodoList update

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,18 @@
 <template>
     <div>
-
+        <ul>
+            <li v-for="(item,idx) in todoItems" :key="idx" >
+                {{ item }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
 import { ref, onBeforeMount } from 'vue'
 const todoItems = ref([])
+
+//life cycle method
 onBeforeMount(() => {
     console.log('mounted in the composition api!')
     if (localStorage.length > 0) {
