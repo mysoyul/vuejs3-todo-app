7edde4d6987adf45e8c19867f99ee1af27a97387
mysoyul <vega2k_@naver.com>
2023-05-09 11:30:34 +0900

    store/index.js loadTodoItems method add

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, createLogger } from "vuex"
 import http from "@/common/http-common"
+import axios from "axios"
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
@@ -7,30 +8,45 @@ export const store = createStore({
     state: {
         todoItems: []
     },
-    actions:{
-
+    actions: {
+        loadTodoItems({ commit }) {
+            http
+                .get('/todos')
+                .then(r => r.data)
+                .then(items => {
+                    commit('setTodoItems', items)
+                })
+                .catch(error => {
+                    if (axios.isAxiosError(error)) {
+                        console.log(error?.response?.status +
+                            ' : ' + error.message)
+                    } else {
+                        console.error(error);
+                    }
+                });
+        },
     },
     mutations: {
         addTodo(state, todoItemStr) {
             const todoItemObj = { completed: false, item: todoItemStr };
             localStorage.setItem(todoItemStr, JSON.stringify(todoItemObj));
-            state.todoItems.push(todoItemObj);      
+            state.todoItems.push(todoItemObj);
         },
         removeTodo(state, payload) {
-            const {todoItem:{item}, index} = payload
+            const { todoItem: { item }, index } = payload
             localStorage.removeItem(item)
-            state.todoItems.splice(index, 1)      
+            state.todoItems.splice(index, 1)
         },
         toggleTodo(state, payload) {
-            const {todoItem:{item, completed}, index} = payload
+            const { todoItem: { item, completed }, index } = payload
             state.todoItems[index].completed = !completed;
-      
+
             localStorage.removeItem(item);
-            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));      
+            localStorage.setItem(item, JSON.stringify(state.todoItems[index]));
         },
         clearTodo(state) {
             localStorage.clear()
             state.todoItems = []
         }
-    },    
+    },
 })
