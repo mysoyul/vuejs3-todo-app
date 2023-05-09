d759184e7772464c2d1d0f9ecc28b46d6df49b9d
mysoyul <vega2k_@naver.com>
2023-05-09 11:26:09 +0900

    store/index.js update

--- src/store/index.js
+++ src/store/index.js
@@ -1,25 +1,14 @@
 import { createStore, createLogger } from "vuex"
-
-const storage = {
-    fetch() {
-        const arr = [];
-        if (localStorage.length > 0) {
-            for (let i = 0; i < localStorage.length; i++) {
-                const itemJson = localStorage.getItem(localStorage.key(i))
-                if (itemJson) {
-                    arr.push(JSON.parse(itemJson))
-                }
-            }
-        }
-        return arr;
-    }
-}
+import http from "@/common/http-common"
 
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
         [createLogger()] : [],
     state: {
-        todoItems: storage.fetch()
+        todoItems: []
+    },
+    actions:{
+
     },
     mutations: {
         addTodo(state, todoItemStr) {
