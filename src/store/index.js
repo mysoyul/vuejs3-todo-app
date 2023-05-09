b90419df62f24569e24a77b0a11ac669869aaa6e
mysoyul <vega2k_@naver.com>
2023-05-09 09:39:04 +0900

    state property add

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,24 @@
 import { createStore, createLogger } from "vuex"
+
+const storage = {
+    fetch() {
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                const itemJson = localStorage.getItem(localStorage.key(i))
+                if (itemJson) {
+                    arr.push(JSON.parse(itemJson))
+                }
+            }
+        }
+        return arr;
+    }
+}
+
 export const store = createStore({
     plugins: process.env.NODE_ENV === 'development' ?
         [createLogger()] : [],
+    state: {
+        todoItems: storage.fetch()
+    },    
 })
